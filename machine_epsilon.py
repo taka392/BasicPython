af321aa59cb243df1ac8d5a79bae792be12cea85
登録したユーザー名 <>
2024-02-21 08:26:28 +0900

    マシンイプシロンを修正しました

--- machine_epsilon.py
+++ machine_epsilon.py
@@ -1,8 +1,13 @@
 # TODO
 
 
-e=1
-while (e + 1) > 1:
-    e=e/2
+
+
+def epsilon():
+    e=1
+    while (e + 1.0) > 1.0:
+        e=e/2
+    return 2*e
     
-print(e)
+result = epsilon()
+print(f"マシンイプシロンの結果は{result}です")

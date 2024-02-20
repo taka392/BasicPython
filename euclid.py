37cd767c47cb924c62eb1da99b8b5c3eb1462c49
登録したユーザー名 <>
2024-02-20 16:15:42 +0900

    ユークリット互除法を追加しました

--- euclid.py
+++ euclid.py
@@ -2,3 +2,17 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
+
+
+
+def eulid(a,b):
+    if a < b:
+        a, b = b, a
+    while b != 0:
+        q = a // b
+        r = a % b
+        a = b
+        b = r
+    
+result = eulid(int(a),int(b))
+print("最大公約数は{}です。".format(a))
